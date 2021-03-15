79162d38f6c63d159b727a31579fe4abbea1f672
master6897 <pucmarcin@gmail.com>
2021-03-15 18:33:36 +0100

    Pierwszy commit.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define ('_SERVER_NAME','localhost');
+define ('_SERVER_URL','http://'._SERVER_NAME);
+define ('_APP_ROOT','/KredytSmarty');
+define ('_APP_URL',_SERVER_URL._APP_ROOT);
+define ("_ROOT_PATH",dirname(__FILE__));
+//& jest wskaźnikiem na zmienną
+/*function out(&$param){
+    if(isset($param)){
+        echo $param;
+    }
+}*/
+?>
